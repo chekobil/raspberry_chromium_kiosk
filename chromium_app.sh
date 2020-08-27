527d894bca734b3290734556ef2a08d4570c161a
kobino <felipe.arg@gmail.com>
2020-08-27 18:14:16 +0200

    ready for Raspberry, add instructions, test on MacOSx

--- chromium_app.sh
+++ chromium_app.sh
@@ -3,7 +3,7 @@
 url=https://airtable.com/shrCjQjwDPteHhKpn/tblTxfk6jjpLo2JFE
 
 #Raspberry OS desktop
-#/usr/bin/chromium-browser --kiosk --fast --fast-start --app=$url &
+/usr/bin/chromium-browser --kiosk --fast --fast-start --app=$url
 
-# MAC OSX 10.15 not fullscreen
-"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --kiosk --fast --fast-start --app="https://airtable.com/shrCjQjwDPteHhKpn/tblTxfk6jjpLo2JFE"
+# MAC OSX 10.15
+#"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --kiosk --fast --fast-start --app=$url
--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #install script on crontab
-#https://stackoverflow.com/questions/4880290/how-do-i-create-a-crontab-through-a-script
-crontab -r 2>/dev/null # BE CAREFUL, removes any previous line on crontab
-#script="*/2 8-19 * * 1,2,3,4,5 /bin/sh /home/pi/raspberry_chromium_kiosk/chromium_app.sh"
+# BE CAREFUL, removes any previous line on crontab
 
-script="*/2 8-19 * * 1,2,3,4,5 /bin/sh /Users/g6/raspberry_chromium_kiosk/chromium_app.sh"
+#https://stackoverflow.com/questions/4880290/how-do-i-create-a-crontab-through-a-script
+crontab -r 2>/dev/null
+# at every hour from 8 to 19, every 2 minutes, on days 1,2,3,4,5 = monday to friday
+script="*/2 8-19 * * 1,2,3,4,5 /bin/sh /home/pi/raspberry_chromium_kiosk/chromium_app.sh"
 (crontab -l 2>/dev/null || true; echo "${script}") | crontab -
