2fd71bf47b57f14e2aefb2336f5408e5c6f56020
kobino <felipe.arg@gmail.com>
2020-08-27 18:00:25 +0200

    problems with install cron

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ crontab -r 2>/dev/null # BE CAREFUL, removes any previous line on crontab
 #script="*/2 8-19 * * 1,2,3,4,5 /bin/sh /home/pi/raspberry_chromium_kiosk/chromium_app.sh"
 
 script="*/2 8-19 * * 1,2,3,4,5 /bin/sh /Users/g6/raspberry_chromium_kiosk/chromium_app.sh"
-(crontab -l 2>/dev/null || true; echo $script) | crontab -
+(crontab -l 2>/dev/null || true; echo "${script}") | crontab -
